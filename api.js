49c8d01d15decac39ae55e7c95111ffb3046b5c7
roomservice <roomservice@yourbtc.net>
2020-05-17 15:27:15 +0200

    initial commit

---
+++ api.js
@@ -0,0 +1,129 @@
+var express = require("express");
+var dhive = require("@hivechain/dhive");
+var signale = require("signale");
+var _ = require("lodash");
+var config = require("./config.json");
+
+var router = express.Router();
+
+let client = new dhive.Client(config.rpc);
+
+router.get("/", (req, res) => {
+  signale.info(new Date(), "- GET request to /api - Source: " + req.ip);
+  res.json({
+    owner_account: config.account,
+  });
+});
+
+router.post("/createAccount", (req, res) => {
+  let auth = req.headers.authority;
+
+  if (!auth) {
+    signale.warn(
+      new Date(),
+      "- POST request to /createAccount - Refused: Authorization header missing. - Source: " +
+        req.ip
+    );
+    res.status(401).send("Authorization header missing.");
+  } else {
+    let provider = _.find(config.auth, { key: auth });
+    if (!provider) {
+      signale.warn(
+        new Date(),
+        "- POST request to /createAccount - Refused: Invalid auth token. - Source: " +
+          req.ip
+      );
+      res.status(401).send("Invalid auth token.");
+    } else {
+      signale.info(
+        new Date(),
+        "- GET request to /api/createAccount - Provider: " +
+          provider.label +
+          " - Source: " +
+          req.ip
+      );
+
+      let name = req.body.name;
+      let publicKeys = req.body.publicKeys;
+      let metaData = req.body.metaData;
+
+      if (
+        typeof name === "undefined" ||
+        typeof publicKeys === "undefined" ||
+        typeof metaData === "undefined"
+      ) {
+        signale.error(
+          new Date(),
+          "- POST request to /createAccount - Error: Body data is invalid or missing. - Source: " +
+            req.ip
+        );
+        res.status(400).send("Body data is invalid or missing.");
+      } else {
+        res.setHeader("Content-Type", "application/json");
+        createAccount(name, publicKeys, metaData).then((response) => {
+          res.send(response);
+        });
+      }
+    }
+  }
+});
+
+function createAccount(name, publicKeys, metaData) {
+  const ownerAuth = {
+    weight_threshold: 1,
+    account_auths: [],
+    key_auths: [[publicKeys.owner, 1]],
+  };
+  const activeAuth = {
+    weight_threshold: 1,
+    account_auths: [],
+    key_auths: [[publicKeys.active, 1]],
+  };
+  const postingAuth = {
+    weight_threshold: 1,
+    account_auths: [],
+    key_auths: [[publicKeys.posting, 1]],
+  };
+
+  let privateKey = dhive.PrivateKey.fromString(config.key);
+
+  let ops = [];
+  const create_op = [
+    "create_claimed_account",
+    {
+      creator: config.account,
+      new_account_name: name,
+      owner: ownerAuth,
+      active: activeAuth,
+      posting: postingAuth,
+      memo_key: publicKeys.memo,
+      json_metadata: JSON.stringify(metaData),
+      extensions: [],
+    },
+  ];
+
+  ops.push(create_op);
+
+  return client.broadcast.sendOperations(ops, privateKey).then(
+    function (result) {
+      signale.success(
+        new Date(),
+        "- OP: create_claimed_account - Success: Account " +
+          name +
+          " was created."
+      );
+      return_status = JSON.stringify({ created: true, name: name });
+      return return_status;
+    },
+    function (error) {
+      signale.error(
+        new Date(),
+        "- OP: create_claimed_account - Error: " + error
+      );
+      return_status = JSON.stringify({ created: false, name: name });
+      return return_status;
+    }
+  );
+}
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,53 @@
+var express = require("express");
+var dhive = require("@hivechain/dhive");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+var signale = require("signale");
+var config = require("./config.json");
+
+var app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/api", require("./api"));
+
+app.listen(config.httpPort, function () {
+  signale.star(
+    "#### Creator-API web server listening on " + config.httpPort + " ####"
+  );
+});
+
+setInterval(claimAccounts, config.autoClaimDelaySeconds * 1000)
+
+async function claimAccounts() {
+  let client = new dhive.Client(config.rpc);
+  let privateKey = dhive.PrivateKey.fromString(config.key);
+
+  try {
+    let ac = await client.call("rc_api", "find_rc_accounts", {
+      accounts: [config.account],
+    });
+
+    let rc = Number(ac.rc_accounts[0].rc_manabar.current_mana);
+
+    if (rc > config.rcThreshold * 1000000000000) {
+      let op = [
+        "claim_account",
+        {
+          creator: config.account,
+          fee: dhive.Asset.from("0.000 HIVE"),
+          extensions: [],
+        },
+      ];
+
+      await client.broadcast.sendOperations([op], privateKey);
+      signale.success(
+        new Date(),
+        "- OP: create_claimed_account - Success: New account ticket was created."
+      );
+    }
+  } catch (error) {
+    signale.error(new Date(), "- OP: claim_account - Error: " + error);
+  }
+}
